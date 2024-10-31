cde34957682960f6ecd9cb1ac7c300893267ffbb
Mariaparma <maria.parma@aluno.senai.br>
2024-10-31 15:54:08 -0300

    feat:create post user method

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -2,16 +2,27 @@ import { Router } from "express";
 import UsersRepository from "../models/users/UsersRepository.js";
 
 const usuariosRoutes = Router();
-const userslist = new UsersRepository
+const userList = new UsersRepository
 
 usuariosRoutes.get("/", (req, res) => {
-  const usuarios =userslist.getAllUsers ();
+  const usuarios =userList.getAllUsers ();
   return res.status(200).json({
     message:
       usuarios.length == 0
         ? "Não há usuários cadastrados"
         : `Total de usuários: ${usuarios.length}`,
     usuarios,
+  });
+});
+
+
+usuariosRoutes.post("/", (req, res) => {
+  const { name, email, password} = req.body;
+
+  const user = userList.addUser(name, email, password);
+
+  return res.status(201).json({
+    massage: "Usuário encontrado com sucesso", user,
   });
 });
 
